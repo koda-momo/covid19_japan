d8b518abe6798bacdf9e3e97d22c4c31d7e1c034
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-07-28 17:14:11 +0900

    エラー解消

--- src/components/top/PrefecturesTable.tsx
+++ src/components/top/PrefecturesTable.tsx
@@ -27,87 +27,90 @@ export const PrefecturesTable: FC = memo(() => {
   return (
     <>
       <table>
-        <tr>
-          <All colSpan={2}>
-            <div>1,246,299 / 112,445</div>
-            <SmallFont>(全国) 現在患者数 / 対策病床数</SmallFont>
-          </All>
-          {prefecturesName.slice(0, 5).map((item) => (
-            <Td key={item.prefectureNumber}>
-              <PrefecturesCell
-                name={item.prefectureName}
-                bed={item.bed}
-                patients={item.patients}
-              />
-            </Td>
-          ))}
-        </tr>
-        <tr>
-          {prefecturesName.slice(5, 12).map((item) => (
-            <Td key={item.prefectureNumber}>
-              <PrefecturesCell
-                name={item.prefectureName}
-                bed={item.bed}
-                patients={item.patients}
-              />
-            </Td>
-          ))}
-        </tr>
-        <tr>
-          {prefecturesName.slice(12, 19).map((item) => (
-            <Td key={item.prefectureNumber}>
-              <PrefecturesCell
-                name={item.prefectureName}
-                bed={item.bed}
-                patients={item.patients}
-              />
-            </Td>
-          ))}
-        </tr>
-        <tr>
-          {prefecturesName.slice(19, 26).map((item) => (
-            <Td key={item.prefectureNumber}>
-              <PrefecturesCell
-                name={item.prefectureName}
-                bed={item.bed}
-                patients={item.patients}
-              />
-            </Td>
-          ))}
-        </tr>
-        <tr>
-          {prefecturesName.slice(26, 33).map((item) => (
-            <Td key={item.prefectureNumber}>
-              <PrefecturesCell
-                name={item.prefectureName}
-                bed={item.bed}
-                patients={item.patients}
-              />
-            </Td>
-          ))}
-        </tr>
-        <tr>
-          {prefecturesName.slice(33, 40).map((item) => (
-            <Td key={item.prefectureNumber}>
-              <PrefecturesCell
-                name={item.prefectureName}
-                bed={item.bed}
-                patients={item.patients}
-              />
-            </Td>
-          ))}
-        </tr>
-        <tr>
-          {prefecturesName.slice(40, 47).map((item) => (
-            <Td key={item.prefectureNumber}>
-              <PrefecturesCell
-                name={item.prefectureName}
-                bed={item.bed}
-                patients={item.patients}
-              />
-            </Td>
-          ))}
-        </tr>
+        <thead></thead>
+        <tbody>
+          <tr>
+            <All colSpan={2}>
+              <div>1,246,299 / 112,445</div>
+              <SmallFont>(全国) 現在患者数 / 対策病床数</SmallFont>
+            </All>
+            {prefecturesName.slice(0, 5).map((item) => (
+              <Td key={item.prefectureNumber}>
+                <PrefecturesCell
+                  name={item.prefectureName}
+                  bed={item.bed}
+                  patients={item.patients}
+                />
+              </Td>
+            ))}
+          </tr>
+          <tr>
+            {prefecturesName.slice(5, 12).map((item) => (
+              <Td key={item.prefectureNumber}>
+                <PrefecturesCell
+                  name={item.prefectureName}
+                  bed={item.bed}
+                  patients={item.patients}
+                />
+              </Td>
+            ))}
+          </tr>
+          <tr>
+            {prefecturesName.slice(12, 19).map((item) => (
+              <Td key={item.prefectureNumber}>
+                <PrefecturesCell
+                  name={item.prefectureName}
+                  bed={item.bed}
+                  patients={item.patients}
+                />
+              </Td>
+            ))}
+          </tr>
+          <tr>
+            {prefecturesName.slice(19, 26).map((item) => (
+              <Td key={item.prefectureNumber}>
+                <PrefecturesCell
+                  name={item.prefectureName}
+                  bed={item.bed}
+                  patients={item.patients}
+                />
+              </Td>
+            ))}
+          </tr>
+          <tr>
+            {prefecturesName.slice(26, 33).map((item) => (
+              <Td key={item.prefectureNumber}>
+                <PrefecturesCell
+                  name={item.prefectureName}
+                  bed={item.bed}
+                  patients={item.patients}
+                />
+              </Td>
+            ))}
+          </tr>
+          <tr>
+            {prefecturesName.slice(33, 40).map((item) => (
+              <Td key={item.prefectureNumber}>
+                <PrefecturesCell
+                  name={item.prefectureName}
+                  bed={item.bed}
+                  patients={item.patients}
+                />
+              </Td>
+            ))}
+          </tr>
+          <tr>
+            {prefecturesName.slice(40, 47).map((item) => (
+              <Td key={item.prefectureNumber}>
+                <PrefecturesCell
+                  name={item.prefectureName}
+                  bed={item.bed}
+                  patients={item.patients}
+                />
+              </Td>
+            ))}
+          </tr>
+        </tbody>
       </table>
     </>
   );

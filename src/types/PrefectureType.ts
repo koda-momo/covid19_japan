72c437c717b043a832523a778b03d1a6b5f85973
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-07-29 19:29:45 +0900

    昨日から減ったか増えたかも判断出来るように修正

--- src/types/PrefectureType.ts
+++ src/types/PrefectureType.ts
@@ -1,7 +1,8 @@
 //都道府県全体の表で使うデータ
 export type PrefecturesType = {
-  prefectureNumber: number;
+  prefectureNumber: string;
   prefectureName: string;
   bed: number; //病床
   patients: number; //現在患者数
+  upDown: "up" | "down"; //昨日より減ったか増えたか(昨日と同じならup)
 };

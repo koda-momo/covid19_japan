96596f1d2d02244efffdb4d7ec859072f7dd3e1d
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-08-01 17:57:49 +0900

    都道府県に円グラフ追加

--- src/templates/Prefectures.tsx
+++ src/templates/Prefectures.tsx
@@ -1,14 +1,55 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { PageButton } from "../components/layout/PageButton";
+
+//chart.js
+import { ChartOptions, ArcElement, Chart, ChartData } from "chart.js";
+import { Pie } from "react-chartjs-2";
+// import ChartDataLabels from "chartjs-plugin-datalabels";
 
 /**
  * 都道府県別表示ページ.
  */
 export const Prefectures: FC = () => {
+  Chart.register(ArcElement);
+  // Chart.register(ChartDataLabels);
+
+  // //円グラフのオプション
+  // const chartOptions: ChartOptions<"pie"> = {
+  //   plugins: {
+  //     datalabels: {
+  //       labels: {
+  //         title: {
+  //           color: "white",
+  //           font: {
+  //             size: 15,
+  //           },
+  //         },
+  //       },
+  //       display: true,
+  //       formatter(value, context) {
+  //         return pieOption(value, context);
+  //       },
+  //     },
+  //   },
+  // };
+
+  //円グラフデータ
+  const [pieData, setPieData] = useState<ChartData<"pie">>({
+    labels: ["現在患者数", "想定病床残数"],
+    datasets: [
+      {
+        label: "鳥取県 現在患者数/対策病床数 564%",
+        data: [564],
+        backgroundColor: ["red", "gray"],
+        hoverOffset: 4,
+      },
+    ],
+  });
+
   return (
     <>
       <PageButton />
-      都道府県別
+      <Pie data={pieData} />
     </>
   );
 };

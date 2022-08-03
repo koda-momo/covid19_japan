738eba41fe1a1922da10636ef6749d8dda0d2305
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-08-03 11:11:28 +0900

    折れ線グラフに緊急搬送データも反映

--- src/hooks/useGetLineGraphData.ts
+++ src/hooks/useGetLineGraphData.ts
@@ -8,21 +8,46 @@ export const useGetLineGraphData = () => {
   const getLineGraphData = useCallback(
     async (
       setDateData: Dispatch<SetStateAction<string[]>>,
+      setEmergencyData: Dispatch<SetStateAction<Array<null | number>>>,
       setNcurrentpatientsData: Dispatch<SetStateAction<number[]>>
     ) => {
-      const response = await axios.get(
+      const response1 = await axios.get(
         "https://www.stopcovid19.jp/data/covid19japan-all.json"
       );
-      const data = response.data;
+
+      const response2 = await axios.get(
+        "https://code4fukui.github.io/fdma_go_jp/emergencytransport_difficult_all.csv"
+      );
+
+      const csv = response2.data.replace(/\n/g, ",");
+      const data2 = csv.replace(/\"/g, "").split(",");
+
+      console.log(data2);
+
+      //緊急搬送データ作成
+      const emergencyArray = new Array<null | number>();
+      //数字の入っている部分が20,23,26...箱目
+      for (let i = 20; i < data2.length; i = i + 3) {
+        emergencyArray.push(Number(data2[i]));
+        //データがない日付分は空ける(6日間)
+        for (let j = 1; j <= 6; j++) {
+          emergencyArray.push(null);
+        }
+      }
+
+      //患者数推移データ作成
+      const data1 = response1.data;
       const ncurrentpatientsArray = new Array<number>();
       const dateArray = new Array<string>();
 
-      for (const item of data) {
-        dateArray.push(item.lastUpdate);
-        ncurrentpatientsArray.push(Number(item.ncurrentpatients));
+      //5月10日のデータから開始
+      for (let i = 53; i < data1.length; i++) {
+        dateArray.push(data1[i].lastUpdate);
+        ncurrentpatientsArray.push(Number(data1[i].ncurrentpatients));
       }
 
       setDateData(dateArray);
+      setEmergencyData(emergencyArray);
       setNcurrentpatientsData(ncurrentpatientsArray);
     },
     []
--- src/templates/LineGraph.tsx
+++ src/templates/LineGraph.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useCallback, useEffect, useState } from "react";
+import { FC, memo, useEffect, useState } from "react";
 import { PageButton } from "../components/layout/PageButton";
 import { LineGraphItem } from "../components/lineGraph/LineGraphItem";
 import { ChartOptions, ChartData } from "chart.js";
@@ -8,10 +8,13 @@ import { useGetLineGraphData } from "../hooks/useGetLineGraphData";
  * 折れ線グラフ表示ページ.
  */
 export const LineGraph: FC = memo(() => {
-  //人数ラベル(y軸)
+  //患者ラベル(y軸)
   const [ncurrentpatientsData, setNcurrentpatientsData] = useState<
     Array<number>
   >([]);
+
+  //緊急搬送ラベル(y軸)
+  const [emergencyData, setEmergencyData] = useState<Array<null | number>>([]);
 
   //日付ラベル(x軸)
   const [dateData, setDateData] = useState<Array<string>>([]);
@@ -20,7 +23,7 @@ export const LineGraph: FC = memo(() => {
   const { getLineGraphData } = useGetLineGraphData();
 
   useEffect(() => {
-    getLineGraphData(setDateData, setNcurrentpatientsData);
+    getLineGraphData(setDateData, setEmergencyData, setNcurrentpatientsData);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -32,6 +35,12 @@ export const LineGraph: FC = memo(() => {
         data: ncurrentpatientsData, //人数(y軸)
         backgroundColor: "#5050CD",
         yAxisID: "ncurrentpatients", //複数データを使用する際はIDを付与
+      },
+      {
+        label: "救急搬送困難事案数",
+        data: emergencyData, //人数(y軸)
+        backgroundColor: "red",
+        yAxisID: "emergency", //複数データを使用する際はIDを付与
       },
     ],
   };
@@ -47,7 +56,7 @@ export const LineGraph: FC = memo(() => {
           text: "現在入院治療を要する者",
         },
       },
-      y: {
+      emergency: {
         display: true,
         position: "left",
         title: {
